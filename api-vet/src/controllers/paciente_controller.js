15f4b293ae3586d40a2a6b7b46a21879b2ca7a61
Nayely Ayol <nayely.ayol@epn.edu.ec>
2025-12-03 15:52:04 -0500

    final.

--- api-vet/src/controllers/paciente_controller.js
+++ api-vet/src/controllers/paciente_controller.js
@@ -1,5 +1,47 @@
-const registrarPaciente = (req, res) =>{
-    res.send("Registro exitoso del paciente")
+import { sendMailToOwner } from "../helpers/sendMail.js"
+import { subirBase64Cloudinary, subirImagenCloudinary } from "../helpers/uploadCloudinary.js"
+import Paciente from "../models/Paciente.js"
+
+const registrarPaciente = async(req,res)=>{
+
+    try {
+        const {emailPropietario} = req.body
+
+        if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Debes llenar todos los campos"})
+
+        const emailExistente = await Paciente.findOne({emailPropietario})
+        
+        if(emailExistente) return res.status(400).json({msg:"El email ya se encuentra registrado"})
+
+        const password = Math.random().toString(36).toUpperCase().slice(2, 5)
+
+        //Registro de un nuevo paciente 
+        const nuevoPaciente = new Paciente({
+            ...req.body,
+            passwordPropietario: await Paciente.prototype.encryptPassword("VET"+password),
+            //Aqui ya se registra al veterinario que va a registrara a este paciente
+            veterinario: req.veterinarioHeader._id
+        })
+
+        if (req.files?.imagen) {
+            const { secure_url, public_id } = await subirImagenCloudinary(req.files.imagen.tempFilePath)
+            nuevoPaciente.avatarMascota = secure_url
+            nuevoPaciente.avatarMascotaID = public_id
+        }
+
+        if (req.body?.avatarMascotaIA) {
+            const secure_url = await subirBase64Cloudinary(req.body.avatarMascotaIA)
+            nuevoPaciente.avatarMascotaIA = secure_url
+        }
+
+        await nuevoPaciente.save()
+        await sendMailToOwner(emailPropietario,"VET"+password)
+        res.status(201).json({ msg: "Registro exitoso de la mascota y correo enviado al propietario" })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ msg: `❌ Error en el servidor - ${error}` })
+    }
 }
 
 
